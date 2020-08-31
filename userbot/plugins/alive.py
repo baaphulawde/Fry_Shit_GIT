0c11ef95998a1052698a94c358966d570ff7580e
baaphulawde <70429842+baaphulawde@users.noreply.github.com>
2020-08-31 15:04:33 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -6,7 +6,7 @@ from userbot import ALIVE_NAME
 from telethon.tl.types import ChannelParticipantsAdmins
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "BAPU"
 PM_IMG = "https://1.bp.blogspot.com/-feE2aIDKae8/X0nXBguWThI/AAAAAAAADgA/U9ZBLPzUvRQYDKj_aqKZLm574SijdUCsACPcBGAsYHg/s0/IMG_20200813_151339_068.jpg"
-pm_caption = "`Bot is :` **ONLINE**\n\n"
+pm_caption = "`Bot is :` **Online and perfectly Updated**\n\n"
 pm_caption += "**I prefer broken bones instead of broken hearts**\n"
 pm_caption += "`TELETHON VERSION:` **1.16.4**\n`Python:` **3.7.9**\n"
 pm_caption += "`DATABASE STATUS:` **Check DATABASEPROPERTYEX Manually**\n"
